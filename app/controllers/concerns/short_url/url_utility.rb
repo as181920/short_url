681c3204a676c9ef0db395efa2a2f1a35ee67093
Andersen Fan <as181920@gmail.com>
2022-03-30 21:50:37 +0800

    Refactor url shorten helper

--- app/controllers/concerns/short_url/url_utility.rb
+++ app/controllers/concerns/short_url/url_utility.rb
@@ -1,11 +1,9 @@
 module ShortUrl
   module UrlUtility
     private
-      def url_with_additional_params(url, params={})
-        uri = URI(url)
-        new_params = URI.decode_www_form(uri.query || '').reject{|k,v| params.stringify_keys.keys.include?(k)} + params.select{|_,v| v.present? }.to_a
-        uri.query = URI.encode_www_form(new_params).presence
-        uri.to_s
+
+      def url_with_additional_params(url, params = {})
+        URI(url).tap { |uri| uri.query = Rack::Utils.parse_nested_query(uri.query).deep_merge(params).to_query.presence }.to_s
       end
   end
 end
