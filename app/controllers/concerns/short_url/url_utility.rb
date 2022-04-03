4972085ebc3e5132176d7fed186c9215eae65851
Andersen Fan <as181920@gmail.com>
2022-04-03 09:46:41 +0800

    Refactor UrlUtility

--- app/controllers/concerns/short_url/url_utility.rb
+++ app/controllers/concerns/short_url/url_utility.rb
@@ -1,9 +1,26 @@
 module ShortUrl
   module UrlUtility
+    extend ActiveSupport::Concern
+
+    included do
+      helper_method :url_with_additional_params
+      helper_method :url_by_shorten
+    end
+
     private
 
       def url_with_additional_params(url, params = {})
         URI(url).tap { |uri| uri.query = Rack::Utils.parse_nested_query(uri.query).deep_merge(params).to_query.presence }.to_s
+      end
+
+      def url_with_stripped_params(url, keys = [])
+        URI(url).tap { |uri| uri.query = Rack::Utils.parse_nested_query(uri.query).except(*keys.map(&:to_s)).to_query.presence }.to_s
+      end
+
+      def url_by_shorten(url, keep_keys: [])
+        shorten_url = ShortUrl::ShortenerService.perform(url_with_stripped_params(url, keep_keys))
+        keep_params = Rack::Utils.parse_nested_query(URI(url).query).slice(*keep_keys.map(&:to_s)).compact_blank
+        URI(shorten_url).tap { |uri| uri.query = keep_params.to_query.presence }.to_s
       end
   end
 end
